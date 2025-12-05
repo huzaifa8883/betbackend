50bc3c301e2b2a022bc068ffc7d074734f331092
huzaifa8883 <aw3540341@gmail.com>
2025-12-05 17:22:35 +0500

    Update markets.js

--- routes/markets.js
+++ routes/markets.js
@@ -1713,7 +1713,7 @@ async function fetchMarketBooks(marketIds) {
 // Polling function
 async function updateHorseCache() {
   try {
-const horseEvents = await fetchEvents(["7"], ["AU", "US", "GB", "ZA"]);
+const horseEvents = await fetchEvents(["7"], ["AU", "US", "GB"]);
 
     if (!horseEvents.length) {
       horseCache = [];
