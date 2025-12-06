74fe376a83678258c9f86f5652a613637f16d22e
huzaifa8883 <aw3540341@gmail.com>
2025-12-07 04:13:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const io = new Server(server, {
 // ✅ ab routes me bhi io use kar sakte ho
 global.io = io;
 
-const PORT = process.env.PORT || config.api.port || 5000;
+const PORT = process.env.PORT || config.api.port || 5001;
 
 /* ---------------- Middleware ---------------- */
 app.use(cors({
